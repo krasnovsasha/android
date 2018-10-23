e11775c04b63725bd2b0a555937c70b0f8022c66
krasnovsasha <pancasancha4@gmail.com>
2018-10-23 15:43:52 +0300

    -Started new Project -added new class Star2DGame -added  baseScreen(Base2DScreen) -added menuScreen(MenuScreen) -added animations for object(redMoon.png)

---
+++ core/src/ru/geekbrains/base/Base2DScreen.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.base;
+
+public class Base2DScreen {
+}
---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.screen;
+
+import ru.geekbrains.base.Base2DScreen;
+
+public class MenuScreen extends Base2DScreen {
+}
